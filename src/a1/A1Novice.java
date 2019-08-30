cc4dd288721974c6ab7bc62b67870edd6eb4b4aa
johnleh <johnleh@johnleh-pc>
2019-08-29 21:31:52 -0400

    potential a1 novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -5,10 +5,51 @@ import java.util.Scanner;
 public class A1Novice {
 
 	public static void main(String[] args) {
-		
+				
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		// Read in count of the number of customers
+		
+		int count = scan.nextInt();
+		
+		
+		// Create an array to store customers
+		
+		String[][] customers = new String[count][3];
+		
+		
+		// input the data for each customer
+		
+		for (int i=0; i<customers.length; i++) {
+			double total = 0.00;
+			customers[i][0] = scan.next();
+			customers[i][1] = scan.next();
+			customers[i][2] = "0.00";
+			int itemsBought = scan.nextInt();
+			for (int k=0; k<itemsBought; k++) {
+				int itemAmount = scan.nextInt();
+				scan.next();
+				double itemPrice = scan.nextDouble();
+				for (int j=0; j<itemAmount;j++) {
+					total += itemPrice;
+				}
+			}
+			
+			customers[i][2] = String.format("%.2f", total);
+
+		}
+		
+		// close the scanner
+		scan.close();
+		
+		for (int i=0; i<customers.length; i++) {
+			System.out.println(customers[i][0].charAt(0) + ". " + customers[i][1] + ": " + customers[i][2]);
+		}
+		
+		
+		// loop to print out the output
+		
+		
 		
 	}
 }
