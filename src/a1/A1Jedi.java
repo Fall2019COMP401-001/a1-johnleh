2b08143e1f120f52a432779a7bf5760c35ac31fe
johnleh <johnleh@johnleh-pc>
2019-08-31 16:41:14 -0400

    maybe finished jedi code?

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -8,7 +8,50 @@ public class A1Jedi {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int numberOfProducts = scan.nextInt();
+		
+		String[] productsName = new String [numberOfProducts];
+		int [][] productsAmount = new int [numberOfProducts][2];
+		
+		for (int i=0; i<numberOfProducts; i++) {
+			productsName[i] = scan.next();
+			scan.next();
+		}
+		
+		int numberOfCustomers = scan.nextInt();
+				
+		for (int i=0; i<numberOfCustomers; i++) {
+			scan.next();
+			scan.next();
+
+			int numberOfItems = scan.nextInt();
+			int numberOfPurchesed;
+
+				for (int j=0; j<numberOfItems; j++) {
+					numberOfPurchesed = scan.nextInt();
+					String nameOfItem = scan.next();
+					for (int k=0;k<productsName.length; k++) {
+						if (productsName[k].equals(nameOfItem)) {
+							productsAmount[k][0] += numberOfPurchesed;
+							productsAmount[k][1] += 1;
+
+						}
+					
+					}	
+				}
+			
+			
+
+		}
+		
+		for(int k=0; k<productsName.length; k++) {
+			if (productsAmount[k][0] == 0) {
+				System.out.println("No customers bought " + productsName[k]);
+			}
+			else {
+				System.out.println(productsAmount[k][1] + " Customers bought " + productsAmount[k][0] + " " + productsName[k]);
+			}
+		}
 		
 	}
 }
